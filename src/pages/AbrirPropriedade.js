bcdd5c2771786e17afb9a57c4842626b3bc3c7cc
EmerHoff <emerson_h2010@hotmail.com>
2020-12-02 23:59:19 -0300

    adicionando mapa

--- src/pages/AbrirPropriedade.js
+++ src/pages/AbrirPropriedade.js
@@ -9,6 +9,7 @@ import {
 
 import api from '../services/api';
 import AsyncStorage from '@react-native-community/async-storage';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 export default class AbrirPropriedade extends React.Component {
   state = {
@@ -23,14 +24,28 @@ export default class AbrirPropriedade extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.menuText}>Propriedade</Text>
-      </View>
+        <MapView
+        style={ styles.map }
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        />
     );
   }
 }
 
 const styles = StyleSheet.create({
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
   container: {
     flex: 1,
     backgroundColor: '#3c9c54',
